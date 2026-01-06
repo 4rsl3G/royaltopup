a76dec1d1c1866a011426325a29a2cfe215d6a56
Jhody Pedia <53709496+4rsl3G@users.noreply.github.com>
2026-01-06 09:15:39 +0700

    Create notfound.partial.js

---
+++ src/views/public/notfound.partial.js
@@ -0,0 +1,11 @@
+<div class="notfound">
+  <div class="box fx-in">
+    <div style="font-size:44px;font-weight:980">404</div>
+    <div class="h1 mt-2">Halaman tidak ditemukan</div>
+    <div class="muted mt-2">Link yang kamu buka tidak tersedia atau sudah berubah.</div>
+    <div class="mt-5 flex gap-2 justify-center">
+      <a class="btn btn-primary" href="/"><i class="ri-home-4-line"></i> Kembali</a>
+      <a class="btn" data-spa="1" href="/p/order"><i class="ri-shopping-bag-3-line"></i> Buat Order</a>
+    </div>
+  </div>
+</div>
